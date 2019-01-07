8c25d1b20e9f9732b8600e6c927629e0beceda24
Leo Mancini <mancini@leo.gd>
2019-01-06 20:00:07 -0800

    Create setup-new-domain.sh

---
+++ setup-new-domain.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+SERVER="[INSERT_SERVER_IP]"
+USER="[INSERT_USERNAME]"
+DOMAINS_DIRECTORY="/home/$USER/domains"
+
+BOLD='\e[1m'
+BOLD_RED='\e[1;31m'
+BOLD_GREEN='\e[1;32m'
+END_COLOR='\e[0m' # End color
+
+echo What is the domain name, including TLD?
+
+read DOMAIN_NAME
+
+if sudo mkdir $DOMAINS_DIRECTORY/$DOMAIN_NAME; then
+        echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created root directory at $DOMAINS_DIRECTORY/$DOMAIN_NAME"
+else
+        echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create root directory at $DOMAINS_DIRECTORY/$DOMAIN_NAME"
+fi
+
+if sudo mkdir $DOMAINS_DIRECTORY/$DOMAIN_NAME/html; then
+        echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created public html directory at $DOMAINS_DIRECTORY/$DOMAIN_NAME/html"
+else
+        echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create public html directory at $DOMAINS_DIRECTORY/$DOMAIN_NAME/html"
+fi
+
+if sudo chown -R $USER $DOMAINS_DIRECTORY/$DOMAIN_NAME; then
+        echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Changed permissions of $DOMAINS_DIRECTORY/$DOMAIN_NAME to $USER"
+else
+        echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot change permissions of $DOMAINS_DIRECTORY/$DOMAIN_NAME to $USER"
+fi
+
+if [ ! -f $DOMAINS_DIRECTORY/$DOMAIN_NAME/html/index.html ]; then
+	touch $DOMAINS_DIRECTORY/$DOMAIN_NAME/html/index.html
+
+	if echo "$DOMAIN_NAME" | sudo tee $DOMAINS_DIRECTORY/$DOMAIN_NAME/html/index.html > /dev/null; then
+	        echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created placeholder index.html at $DOMAINS_DIRECTORY/$DOMAIN_NAME/html/index.html"
+	else
+	        echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create placeholder index.html at $DOMAINS_DIRECTORY/$DOMAIN_NAME/html/index.html"
+	fi
+fi
+
+sudo touch /etc/apache2/sites-available/$DOMAIN_NAME.conf
+
+if echo "<VirtualHost *:80>
+    ServerName $DOMAIN_NAME
+    ServerAlias www.$DOMAIN_NAME
+    ServerAdmin www@noshado.ws
+    DocumentRoot $DOMAINS_DIRECTORY/$DOMAIN_NAME/html
+    <Directory $DOMAINS_DIRECTORY/$DOMAIN_NAME/html>
+            AllowOverride all
+            Require all granted
+    </Directory>
+
+    ErrorLog /var/log/apache2/$DOMAIN_NAME-error.log
+    CustomLog /var/log/apache2/$DOMAIN_NAME-access.log combined
+</VirtualHost>" | sudo tee /etc/apache2/sites-available/$DOMAIN_NAME.conf > /dev/null; then
+	echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created Apache config file at /etc/apache2/sites-available/$DOMAIN_NAME.conf"
+else
+	echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create Apache config file at /etc/apache2/sites-available/$DOMAIN_NAME.conf"
+fi
+
+if sudo a2ensite $DOMAIN_NAME > /dev/null; then
+	echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Enabled site $DOMAIN_NAME in Apache"
+else
+	echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot enable site $DOMAIN_NAME in Apache"
+fi
+
+if sudo service apache2 reload; then
+	echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Reloaded Apache"
+else
+	echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot reload Apache"
+fi
+
+read -r -p "Do you want to setup HTTPS with Let's Encrypt? [y/N] " ssl_certificate_setup_response
+if [[ "$ssl_certificate_setup_response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+	echo "Requesting certificate from Let's Encrypt for $DOMAIN_NAME"
+	/opt/certbot/certbot-auto --apache -d $DOMAIN_NAME
+	
+	HTTPS="true"
+else
+	echo -e "${BOLD}PASSED${END_COLOR} Not setting up HTTPS for $DOMAIN_NAME"
+	
+	HTTPS="false"
+fi
+
+read -r -p "Do you want to setup a Git repository for $DOMAIN_NAME? [y/N] " git_setup_response
+if [[ "$git_setup_response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+	
+	if sudo rm $DOMAINS_DIRECTORY/$DOMAIN_NAME/html/index.html; then
+		echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Removed placeholder index.html at $DOMAINS_DIRECTORY/$DOMAIN_NAME/html/index.html"
+	else
+		echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot remove placeholder index.html at $DOMAINS_DIRECTORY/$DOMAIN_NAME/html/index.html"
+	fi
+	
+	if git init --bare $DOMAINS_DIRECTORY/$DOMAIN_NAME/$DOMAIN_NAME.git; then
+		echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created empty Git repository at $DOMAINS_DIRECTORY/$DOMAIN_NAME/$DOMAIN_NAME.git"
+	else
+		echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create empty Git repository at $DOMAINS_DIRECTORY/$DOMAIN_NAME/$DOMAIN_NAME.git"
+	fi
+	
+	sudo touch $DOMAINS_DIRECTORY/$DOMAIN_NAME/$DOMAIN_NAME.git/hooks/post-receive
+	sudo chmod +x $DOMAINS_DIRECTORY/$DOMAIN_NAME/$DOMAIN_NAME.git/hooks/post-receive
+	
+	if echo "#!/bin/bash
+echo -e \"${BOLD_GREEN}SUCCESS${END_COLOR} Deployed master to $DOMAINS_DIRECTORY/$DOMAIN_NAME/html\"
+git --work-tree=$DOMAINS_DIRECTORY/$DOMAIN_NAME/html --git-dir=$DOMAINS_DIRECTORY/$DOMAIN_NAME/$DOMAIN_NAME.git checkout -f" | sudo tee $DOMAINS_DIRECTORY/$DOMAIN_NAME/$DOMAIN_NAME.git/hooks/post-receive > /dev/null; then
+		echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created post-receive hook at $DOMAINS_DIRECTORY/$DOMAIN_NAME/$DOMAIN_NAME.git/hooks/post-receive"
+	else
+		echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create post-receive hook at $DOMAINS_DIRECTORY/$DOMAIN_NAME/$DOMAIN_NAME.git/hooks/post-receive"
+	fi
+	
+	GIT="true"
+else
+	echo -e "${BOLD}PASSED${END_COLOR} Not setting up Git repository for $DOMAIN_NAME"
+	
+	GIT="false"
+fi
+
+echo -e "\n------------------------------------"
+echo -e "--------------- ${BOLD}DONE${END_COLOR} ---------------"
+echo -e "------------------------------------ \n"
+
+echo -e "${BOLD}*** $DOMAIN_NAME is now set up! ***${END_COLOR}\n"
+
+if [[ $HTTPS == "true" ]]; then
+	echo -e "* Visit ${BOLD}https://$DOMAIN_NAME${END_COLOR} to see the new site"
+else
+	echo -e "* Visit ${BOLD}http://$DOMAIN_NAME${END_COLOR} to see the new site"
+fi
+
+if [[ $GIT == "true" ]]; then
+	echo -e "\n* Add this remote to deploy: \n${BOLD}git remote add web $USER@$SERVER:$DOMAINS_DIRECTORY/$DOMAIN_NAME/$DOMAIN_NAME.git${END_COLOR}"
+fi
+
+echo -e " "
