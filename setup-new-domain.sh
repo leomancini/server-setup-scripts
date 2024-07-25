8aa0fa471981475ce5f4f47dc35a7f11eecd0191
Leo Mancini <mancini@leo.gd>
2024-07-24 21:18:19 -0400

    Updates debug language

--- setup-new-domain.sh
+++ setup-new-domain.sh
@@ -101,10 +101,10 @@ else
 	echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot reload Apache"
 fi
 
-# Optional: Request SSL certificate from Let's Encrypt
+# Optional: Generate SSL certificate from Let's Encrypt
 read -r -p "Do you want to setup HTTPS with Let's Encrypt? [y/N] " SSL_CERTIFICATE_SETUP
 if [[ "$SSL_CERTIFICATE_SETUP" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
-	echo "Requesting certificate from Let's Encrypt for $DOMAIN_NAME"
+	echo "Generating certificate with Let's Encrypt for $DOMAIN_NAME"
 	sudo certbot --apache -d $DOMAIN_NAME,www.$DOMAIN_NAME
 	
 	HTTPS="true"
