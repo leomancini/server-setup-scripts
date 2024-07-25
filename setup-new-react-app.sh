01759d856e59d27bab32aef3c68a7883151a43d2
Leo Mancini <mancini@leo.gd>
2024-07-25 01:56:32 -0400

    Create setup-new-react-app.sh

---
+++ setup-new-react-app.sh
@@ -0,0 +1,350 @@
+#!/bin/bash
+
+# Set up variables for server IP, user, and domains directory
+SERVER="[INSERT_SERVER_IP]"
+USER="[INSERT_USERNAME]"
+ADMIN_CONTACT="[INSERT_ADMIN_CONTACT]"
+APPS_DIRECTORY="/home/$USER/react-apps"
+
+# Set up formatting for use later
+BOLD='\e[1m'
+BOLD_RED='\e[1;31m'
+BOLD_GREEN='\e[1;32m'
+END_COLOR='\e[0m' # This ends formatting
+
+read -p "App Name: " APP_DISPLAY_NAME
+read -p "App ID: " APP_ID
+read -p "URL: " DOMAIN_NAME
+
+echo -e " "
+
+# Create root directory for app
+if sudo mkdir $APPS_DIRECTORY/$APP_ID; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created root directory at $APPS_DIRECTORY/$APP_ID"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create root directory at $APPS_DIRECTORY/$APP_ID"
+fi
+
+# Create src directory for app
+if sudo mkdir $APPS_DIRECTORY/$APP_ID/src; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created src directory at $APPS_DIRECTORY/$APP_ID/src"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create src directory at $APPS_DIRECTORY/$APP_ID/src"
+fi
+
+# Create public directory for app
+if sudo mkdir $APPS_DIRECTORY/$APP_ID/public; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created public directory at $APPS_DIRECTORY/$APP_ID/public"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create public directory at $APPS_DIRECTORY/$APP_ID/public"
+fi
+
+# Create a basic src/App.js file
+sudo touch $APPS_DIRECTORY/$APP_ID/src/App.js
+if echo "import React from \"react\";
+import styled from \"styled-components\";
+
+const Page = styled.div\`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 24px;
+  color: #333;
+\`;
+
+function App() {
+  return <Page>Hello world!</Page>;
+}
+
+export default App;
+" | sudo tee $APPS_DIRECTORY/$APP_ID/src/App.js > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created basic src/App.js file"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create basic src/App.js file"
+fi
+
+# Create a basic src/index.css
+sudo touch $APPS_DIRECTORY/$APP_ID/src/index.css
+if echo "body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, \"Segoe UI\", \"Roboto\", \"Oxygen\",
+    \"Ubuntu\", \"Cantarell\", \"Fira Sans\", \"Droid Sans\", \"Helvetica Neue\",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, \"Courier New\",
+    monospace;
+}
+" | sudo tee $APPS_DIRECTORY/$APP_ID/src/index.css > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created basic src/index.css file"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create basic src/index.css file"
+fi
+
+# Create a basic src/index.js
+sudo touch $APPS_DIRECTORY/$APP_ID/src/index.js
+if echo "import React from \"react\";
+import ReactDOM from \"react-dom/client\";
+import \"./index.css\";
+import App from \"./App\";
+
+const root = ReactDOM.createRoot(document.getElementById(\"root\"));
+root.render(<App />);
+" | sudo tee $APPS_DIRECTORY/$APP_ID/src/index.js > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created basic src/index.js file"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create basic src/index.js file"
+fi
+
+# Create a basic public/index.html
+sudo touch $APPS_DIRECTORY/$APP_ID/public/index.html
+if echo "<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset=\"utf-8\" />
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />
+    <title>$APP_DISPLAY_NAME</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id=\"root\"></div>
+  </body>
+</html>
+" | sudo tee $APPS_DIRECTORY/$APP_ID/public/index.html > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created basic public/index.html file"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create basic public/index.html file"
+fi
+
+# Create a basic public/manifest.json
+sudo touch $APPS_DIRECTORY/$APP_ID/public/manifest.json
+if echo "{
+  \"short_name\": \"$APP_DISPLAY_NAME\",
+  \"name\": \"$APP_DISPLAY_NAME\",
+  \"start_url\": \".\",
+  \"display\": \"standalone\",
+  \"theme_color\": \"#000000\",
+  \"background_color\": \"#ffffff\"
+}
+" | sudo tee $APPS_DIRECTORY/$APP_ID/public/manifest.json > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created basic public/manifest.json file"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create basic public/manifest.json file"
+fi
+
+# Create a basic public/robots.txt
+sudo touch $APPS_DIRECTORY/$APP_ID/public/manifest.json
+if echo "# https://www.robotstxt.org/robotstxt.html
+User-agent: *
+Disallow:
+" | sudo tee $APPS_DIRECTORY/$APP_ID/public/robots.txt > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created basic public/robots.txt file"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create basic public/robots.txt file"
+fi
+
+# Create a basic README.md
+sudo touch $APPS_DIRECTORY/$APP_ID/README.md
+if echo "# $APP_DISPLAY_NAME
+" | sudo tee $APPS_DIRECTORY/$APP_ID/README.md > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created basic README.md file"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create basic README.md file"
+fi
+
+# Create a basic package.json
+sudo touch $APPS_DIRECTORY/$APP_ID/package.json
+if echo "{
+  \"name\": \"$APP_ID\",
+  \"homepage\": \"./\",
+  \"version\": \"0.1.0\",
+  \"private\": false,
+  \"dependencies\": {
+    \"react\": \"^18.3.1\",
+    \"react-dom\": \"^18.3.1\",
+    \"react-scripts\": \"5.0.1\",
+    \"styled-components\": \"^6.1.11\"
+  },
+  \"scripts\": {
+    \"start\": \"react-scripts start\",
+    \"build\": \"react-scripts build\",
+    \"test\": \"react-scripts test\",
+    \"eject\": \"react-scripts eject\"
+  },
+  \"eslintConfig\": {
+    \"extends\": [
+      \"react-app\",
+      \"react-app/jest\"
+    ]
+  },
+  \"browserslist\": {
+    \"production\": [
+      \">0.2%\",
+      \"not dead\",
+      \"not op_mini all\"
+    ],
+    \"development\": [
+      \"last 1 chrome version\",
+      \"last 1 firefox version\",
+      \"last 1 safari version\"
+    ]
+  },
+  \"devDependencies\": {
+    \"@babel/plugin-proposal-private-property-in-object\": \"^7.21.11\"
+  }
+}
+" | sudo tee $APPS_DIRECTORY/$APP_ID/package.json > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created basic package.json file"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create basic package.json file"
+fi
+
+# Create a setup-log.json
+sudo touch $APPS_DIRECTORY/$APP_ID/setup-log.json
+if echo "{
+  \"app_id\": \"$APP_ID\",
+  \"app_name\": \"$APP_DISPLAY_NAME\",
+  \"domain\": \"https://$DOMAIN_NAME\",
+  \"author\": \"$USER\",
+  \"created_on\": \"$(date)\",
+}
+" | sudo tee $APPS_DIRECTORY/$APP_ID/setup-log.json > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created setup-log.json file"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create setup-log.json file"
+fi
+
+# Change permissions for app directory to specified user
+if sudo chown -R $USER $APPS_DIRECTORY/$APP_ID; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Changed permissions to set $USER as owner"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot change permissions to set $USER as owner"
+fi
+
+# Install node modules
+if cd $APPS_DIRECTORY/$APP_ID && npm install; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Installed node modules"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot install node modules"
+fi
+
+# Build app for production
+if cd $APPS_DIRECTORY/$APP_ID && npm run build; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Built app for production"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot build app for production"
+fi
+
+# Create a VirtualHost config file that points to the app's build directory
+sudo touch /etc/apache2/sites-available/$DOMAIN_NAME.conf
+if echo "<VirtualHost *:80>
+    
+    ServerName $DOMAIN_NAME
+    ServerAlias www.$DOMAIN_NAME
+    ServerAdmin $ADMIN_CONTACT
+
+    DocumentRoot $APPS_DIRECTORY/$APP_ID/build
+    
+    <Directory $APPS_DIRECTORY/$APP_ID/build>
+        AllowOverride all
+        Require all granted
+    </Directory>
+
+    ErrorLog /var/log/apache2/$DOMAIN_NAME-error.log
+    CustomLog /var/log/apache2/$DOMAIN_NAME-access.log combined
+    
+</VirtualHost>" | sudo tee /etc/apache2/sites-available/$DOMAIN_NAME.conf > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created Apache config file at /etc/apache2/sites-available/$DOMAIN_NAME.conf"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create Apache config file at /etc/apache2/sites-available/$DOMAIN_NAME.conf"
+fi
+
+# Enable site in Apache
+if sudo a2ensite $DOMAIN_NAME > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Enabled site in Apache"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot enable site in Apache"
+fi
+
+# Reload Apache
+if sudo service apache2 reload; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Reloaded Apache"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot reload Apache"
+fi
+
+# Generate SSL certificate with Let's Encrypt
+echo "Generating certificate with Let's Encrypt for $DOMAIN_NAME"
+if sudo certbot --apache -d $DOMAIN_NAME,www.$DOMAIN_NAME; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Generated SSL certificate"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot generate SSL certificate"
+fi
+
+# Initialize Git repository
+if cd $APPS_DIRECTORY/$APP_ID && \
+    git init && \
+    git checkout -b main && \
+    git config receive.denyCurrentBranch updateInstead > /dev/null 2>&1; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created git repository"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create git repository"
+fi
+
+# Create basic gitignore file
+sudo touch $APPS_DIRECTORY/$APP_ID/.gitignore
+if echo '.env
+node_modules/
+build/
+output.log
+' | sudo tee $APPS_DIRECTORY/$APP_ID/.gitignore > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created basic gitignore file"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create basic gitignore file"
+fi
+
+# Commit basic code
+if git add . && git commit -m "first commit" > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Commited initial code to repository"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot initial code to repository"
+fi
+
+# Set up a hook that deploys any commits made to this repo 
+sudo touch $APPS_DIRECTORY/$APP_ID/.git/hooks/post-receive
+sudo chmod +x $APPS_DIRECTORY/$APP_ID/.git/hooks/post-receive
+sudo chown $USER $APPS_DIRECTORY/$APP_ID/.git/hooks/post-receive
+
+if echo "#!/bin/bash
+
+cd "$APPS_DIRECTORY/$APP_ID" || { echo "Failed to change directory"; exit 1; }
+
+echo "Installing dependencies"
+npm install || { echo "npm install failed"; exit 1; }
+
+echo "Building app for production"
+npm run build > ./output.log 2>&1 &
+
+echo -e \"${BOLD_GREEN}SUCCESS${END_COLOR} Deployed main to $APPS_DIRECTORY/$APP_ID\"
+" | sudo tee $APPS_DIRECTORY/$APP_ID/.git/hooks/post-receive > /dev/null; then
+    echo -e "${BOLD_GREEN}SUCCESS${END_COLOR} Created post-receive hook"
+else
+    echo -e "${BOLD_RED}FAILED${END_COLOR} Cannot create post-receive hook"
+fi
+
+# Show confirmation messages depending on optional steps
+echo -e "\n------------------------------------"
+echo -e "--------------- ${BOLD}DONE${END_COLOR} ---------------"
+echo -e "------------------------------------ \n"
+
+echo -e "${BOLD}*** $APP_ID is now set up! ***${END_COLOR}\n"
+
+echo -e "* Visit ${BOLD}https://$DOMAIN_NAME${END_COLOR} to see the new site"
+
+echo -e "\n* Clone this repository and push to origin to deploy: \n${BOLD}git clone $USER@$SERVER:$APPS_DIRECTORY/$APP_ID${END_COLOR}"
+
+echo -e " "
